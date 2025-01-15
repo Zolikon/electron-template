9b4e35db6795e349192a75a1f97c794231cfc315
Zoltan Pozsonyi <pozsonyizoli@gmail.com>
2025-01-15 18:18:58 +0100

    adding query message to react side

--- src/renderer/src/useElectron.js
+++ src/renderer/src/useElectron.js
@@ -1,3 +1,13 @@
 export function useElectron() {
-  return { electron: window.electron, eventBus: window.electron.ipcRenderer }
+  function query(event, onResponse) {
+    window.electron.ipcRenderer.once(event, (_, response) => {
+      onResponse(response)
+    })
+    window.electron.ipcRenderer.send(event)
+  }
+
+  return {
+    electron: window.electron,
+    eventBus: { ...window.electron.ipcRenderer, query },
+  }
 }
--- src/renderer/src/useSubsribe.js
+++ src/renderer/src/useSubsribe.js
@@ -1,15 +1,22 @@
-import { useEffect } from 'react'
-import { useElectron } from './useElectron'
+import { useEffect } from "react";
+import { useElectron } from "./useElectron";
 
-export function useSubscribe(message, onEventTriggered) {
-  const { eventBus } = useElectron()
+export function useSubscribe(message, onEventTriggered, once = false) {
+  const { eventBus } = useElectron();
   useEffect(() => {
-    const completedListener = eventBus.on(message, (event, status) => {
-      onEventTriggered(status)
-    })
+    let completedListener;
+    if (once) {
+      completedListener = eventBus.once(message, (event, status) => {
+        onEventTriggered(status);
+      });
+    } else {
+      completedListener = eventBus.on(message, (event, status) => {
+        onEventTriggered(status);
+      });
+    }
 
     return () => {
-      eventBus.removeListener(message, completedListener)
-    }
-  }, [eventBus])
+      eventBus.removeListener(message, completedListener);
+    };
+  }, [eventBus]);
 }
